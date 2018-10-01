613ef4c5bbf946a9bc5acda2798d86b38369eb0e
nmarshall <nmarshall@ATC-137-PD11D.sau40.com>
2018-10-01 14:04:46 -0400

    TradeOrder Done Maybe

--- Stock Exchange/src/stockExchange/TradeOrder.java
+++ Stock Exchange/src/stockExchange/TradeOrder.java
@@ -1,9 +1,46 @@
 package stockExchange;
 
 public class TradeOrder {
+	
+	private Trader myTrader;
+	private String symbol;
+	private boolean buyOrder, marketOrder;
+	private int numShares;
+	private double price;
 
 	public TradeOrder(Trader myTrader, String symbol, boolean buyOrder, boolean marketOrder, int numShares, double price) {
+		
+		this.myTrader = myTrader;
+		this.symbol = symbol;
+		this.buyOrder = buyOrder;
+		this.marketOrder = marketOrder;
+		this.numShares = numShares;
+		this.price = price;
+		
+	}
 	
+	public Trader getMyTrader(){
+		return myTrader;
+	}
+	
+	public String getSymbol(){
+		return symbol;
+	}
+	
+	public boolean getBuyOrder(){
+		return buyOrder;
+	}
+	
+	public boolean getMarketOrder(){
+		return marketOrder;
+	}
+	
+	public int getNumShares(){
+		return numShares;
+	}
+	
+	public double getPrice(){
+		return price;
 	}
 
 }
