774e66c5e8661bf1c3fdf752e8a02046f0204397
nmarshall <nmarshall@ATC-137-PD11D.sau40.com>
2018-10-02 13:27:08 -0400

    Trader

--- Stock Exchange/src/stockExchange/Trader.java
+++ Stock Exchange/src/stockExchange/Trader.java
@@ -2,11 +2,40 @@ package stockExchange;
 
 import java.awt.GraphicsConfiguration;
 
-public class Trader {
-
-	public GraphicsConfiguration getName() {
+public class Trader extends java.lang.Object implements java.lang.Comparable<Trader> {
+	
+	private Brokerage brokerage;
+	private String name, password;
+	private String[] messages;
+	
+	public Trader(Brokerage brokerage, String name, String pswd){
 		
-		return null;
+		this.brokerage = brokerage;
+		this.name = name;
+		password = pswd;
+		
+	}
+	
+	public boolean equals(Trader other){
+		
+		return name.equals(other.name);
+		
+	}
+
+	public String getName() {
+		
+		return name;
+	}
+	
+	public String getPassword(){
+		
+		return password;
+	}
+	
+	public boolean hasMessages(){
+		if(messages.length == 0)
+			return false;
+		return true;
 	}
 
 	public void quit() {
@@ -19,6 +48,11 @@ public class Trader {
 
 	public void placeOrder(TradeOrder tradeOrder) {
 		
+	}
+
+	public int compareTo(Trader other) {
+		
+		return 0;
 	}
 
 }
