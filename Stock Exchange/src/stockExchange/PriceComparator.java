9c223c0a042e7fda88645819233153d58844355a
npelletier <npelletier@ATC-137-PD12D.sau40.com>
2018-10-02 13:51:28 -0400

    Finished(?) PriceComparator.

--- Stock Exchange/src/stockExchange/PriceComparator.java
+++ Stock Exchange/src/stockExchange/PriceComparator.java
@@ -4,17 +4,32 @@ import java.util.*;
 
 public class PriceComparator {
 	
-	private PriorityQueue<TradeOrder> queue;
+	boolean asc = true;
 	
-	public PriceComparator(){
-		
-	}
+	public PriceComparator(){}
 	
 	public PriceComparator(boolean asc){
-		
+		this.asc = asc;
 	}
 	
 	public int compare(TradeOrder order1, TradeOrder order2){
-		return -1;
+		double diff = order1.getPrice() - order2.getPrice();
+		if(asc){
+			if(diff < 0){
+				return 1;
+			}else if(diff > 0){
+				return -1;
+			}else{
+				return 0;
+			}
+		}else{
+			if(diff < 0){
+				return -1;
+			}else if(diff > 0){
+				return 1;
+			}else{
+				return 0;
+			}
+		}
 	}
 }
