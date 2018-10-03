984a4ac1db5428be4f3f459f988cf0270d1eb8a5
cliamos <cliamos@ATC-137-PD14D.sau40.com>
2018-10-03 13:32:06 -0400

    changed brokerage

--- Stock Exchange/src/stockExchange/Brokerage.java
+++ Stock Exchange/src/stockExchange/Brokerage.java
@@ -1,18 +1,42 @@
 package stockExchange;
 
-public class Brokerage implements Login {
-	
-	
-	public Brokerage(StockExchange exchange) {
+import java.util.*;
+public class Brokerage implements Login
+{
+	TreeMap<String, Trader> TM;
+	public Brokerage(StockExchange exchange)
+	{
+		TM = new TreeMap<String, Trader>();
+	}
+	public int addUser(java.lang.String name, java.lang.String password)
+	{
+		Set<String> s1 = TM.keySet();
 		
+		for(String s2: s1)
+		{
+			if(name == TM.get(s2).getName())
+			{
+				return -3;
+			}
+		}
+		if (name.length() < 4 || name.length() > 10)
+			return -1;
+		else if (password.length() < 2 || password.length() > 10)
+			return -2;
+		else
+		{
+			TM.put(name, new Trader(this, name, password));
+			return 0;
+		}
 	}
 
-	public int addUser(String string, String string2) {
-		return -1;
+	public int login(String name, String password)
+	{
 	}
-
-	public int login(String string, String string2) {
-		return -1;
+	
+	public void getQuote(Trader trader, java.lang.String symbol)
+	{
+		
 	}
 
 }
